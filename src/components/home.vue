e8eb0a6ff190696dedef7837ae690826ad9dabff
寇佳龙 <jialongkou@iyuedian.com>
2020-11-15 23:57:11 +0800

    菜单

--- src/components/home.vue
+++ src/components/home.vue
@@ -5,7 +5,7 @@
             <el-button type="info" @click="logout">退出</el-button>
         </el-header>
         <el-container>
-            <el-aside width="200px">
+            <el-aside width="250px">
                 <el-menu
                         default-active="2"
                         class="el-menu-vertical-demo"
@@ -15,15 +15,17 @@
                         text-color="#fff"
                         active-text-color="#ffd04b">
                     <!--一级菜单的模板区域-->
-                    <el-submenu index="1">
+                    <el-submenu :key="item.id" v-if="item.children[0]" index="1"
+                                v-for="(item,index) in menuData" :index="index+1+''">
                         <template slot="title">
                             <!--图标-->
-                            <i class="el-icon-location"></i>
-                            <span>导航一</span>
+                            <i class="el-icon-menu"></i>
+                            <span>{{item.name}}</span>
                         </template>
-                        <el-menu-item index="1-4-1">
-                            <i class="el-icon-location"></i>
-                            <span>导航一</span>
+                        <el-menu-item v-for="child in item.children"
+                                      :index="child.url" :key="item.id">
+<!--                            <i class="el-icon-location"></i>-->
+                            <span>{{child.name}}</span>
                         </el-menu-item>
                     </el-submenu>
                 </el-menu>
@@ -36,11 +38,65 @@
 <script>
   export default {
     name: 'home',
+    data(){
+      return{
+        menuData:[]
+      }
+    },
     methods: {
       logout () {
         window.sessionStorage.clear()
         this.$router.push('/login')
+      },
+      async getMenu(){
+        const {data:res}=await this.$http.post("manage/sysRoleMenu/menu/1")
+        console.log(res)
+        //装载所有导航
+        let att = []
+        for (let i=0;i<res.data.length;i++){
+          att.push(res.data[i].sysMenu)
+        }
+        if (att==''){
+          that.$message('暂无使用权限');
+          this.$router.push({
+            path: '/login', // 退出登录
+          })
+        }
+        //父级菜单
+        let arr = [];
+        //子级菜单
+        let tow = [];
+        //总菜单
+        let arrList = [];
+        //循环出需要的数
+        for (let i=0;i<att.length;i++){
+          //默认是不选
+          if (att[i].type==1) {
+            att[i].children = []
+            arr.push(att[i])
+          }else {
+            tow.push(att[i])
+          }
+          arrList.push(att[i])
+        }
+        for (let i=0;i<arr.length;i++){
+          for (let l=0;l<tow.length;l++){
+            if (tow[l].parentId==arr[i].id){
+              let show = arr[i].children.filter(item=>{
+                return item.id === tow[l].id
+              })
+              if(show.length===0){
+                arr[i].children.push(tow[l])
+              }
+            }
+          }
+        }
+        console.log(arr)
+        this.menuData=arr;
       }
+    },
+    mounted() {
+      this.getMenu();
     }
   }
 </script>
